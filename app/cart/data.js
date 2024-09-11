0bbf0c31a704901fca516259cb166115216c8625
김소연 <ksoyeon31@mediazen.co.kr>
2024-09-11 17:57:56 +0900

    second commit

---
+++ app/cart/data.js
@@ -0,0 +1,5 @@
+let age = 20;
+
+export default age;  //export는 내보낸다는 뜻
+//export default는 파일마다 한번씩만 사용가능 만약 여러개의 변수나 함수를 export 하고 싶다면
+//export {변수1, 변수2} 이런식으로 작성해야함 > 그럼 import할때도 import {변수1, 변수2} 이런식으로 똑같이 하면 됨
--- app/cart/page.js
+++ app/cart/page.js
@@ -1,12 +1,21 @@
-//맨 위에 'use client'를 작성하는 순간 밑에 코드들은 클라이언트 컴포넌트가 됨
-// 중간에 끼워
+// 맨 위에 'use client'를 작성하는 순간 밑에 코드들은 클라이언트 컴포넌트가 됨
+// 중간에 끼워넣는거 안됨
 // 이 밑에는 기본적으로 다 서버 컴포넌트임
+// 둘의 차이는 서버 컴포넌트에선 html 코드 중간에 js 기능을 넣는게 불가능함 ex useState, useEffect, onClick같은 것들
+// 근데 client 컴포넌트에선 js 기능을 넣을 수 있음
+// 대신 서버 컴포넌트는 로딩속도가 빠름 왜냐 js기능이 없기 때문 또한 검색엔진 노출 유리
+// client 컴포넌트는 로딩속도가 느림 1. js 많이 필요 2. hydration이라는 과정을 거쳐야함(리액트 문법을 쓰기위해서 html을 유저에게 쏴주고 그 html을 리액트로 분석하는 과정)
+
+// 큰 페이지는 server component로 , js 기능 필요한 곳만 client component
+
+import age from './data.js';
+
 export default function Cart(){
     return(
         <div>
         <h4 className="title">Cart</h4>
         <div className="cart-item">
-          <p>상품명</p>
+          <p>상품명 {age}</p>
           <p>$40</p>
           <p>1개</p>
         </div>
